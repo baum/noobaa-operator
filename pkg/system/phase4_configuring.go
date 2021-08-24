530a9c90efa3855da73ea9a1ec68bbc68ae724f7
Alexander Indenbaum <aindenba@redhat.com>
2021-08-24 14:24:04 +0300

    Refactor SCC/RBAC rebase 18 Apr 2022
    1. Noobaa operator no longer uses 'noobaa' SCC and relies on RBAC only.
    2. Handle SCC RBAC creation for DB and Endpoint inside operator's
       reconcile loop.
    3. OCS operator interface:
       a. OCS operator no longer needs to create 'noobaa' & 'noobaa-endpoint'
          SCCs. newNooBaaSCC() and newNooBaaEndpointSCC() functions could be
          removed from the OCS codebase.
       b. Use ''noobaa olm csv' to pass operator's RBAC resources to OCS.
    4. Create a rbac/scc for the db.
    
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

--- pkg/system/phase4_configuring.go
+++ pkg/system/phase4_configuring.go
@@ -64,6 +64,9 @@ func (r *Reconciler) ReconcilePhaseConfiguring() error {
 	if err := r.ReconcileSystemSecrets(); err != nil {
 		return err
 	}
+	if err := r.reconcileEndpointRBAC(); err != nil {
+		return err
+	}
 	if err := r.ReconcileObject(r.DeploymentEndpoint, r.SetDesiredDeploymentEndpoint); err != nil {
 		return err
 	}
@@ -1522,4 +1525,13 @@ func (r *Reconciler) ReconcileNamespaceStores(namespaceResources []nb.NamespaceR
 		}
 	}
 	return nil
+}
+
+// reconcileEndpointRBAC creates Endpoint scc, role, rolebinding and service account
+func (r* Reconciler) reconcileEndpointRBAC() error {
+	return r.reconcileRbac(
+		bundle.File_deploy_scc_endpoint_yaml,
+		bundle.File_deploy_service_account_endpoint_yaml,
+		bundle.File_deploy_role_endpoint_yaml,
+		bundle.File_deploy_role_binding_endpoint_yaml)
 }
